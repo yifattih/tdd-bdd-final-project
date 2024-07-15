6c822616cba6f07262f3607284bdb884ff567b96
yifattih <yifattih@proton.me>
2024-07-15 15:25:20 -0400

    microservice: test_find_by_name implemented

--- tests/test_models.py
+++ tests/test_models.py
@@ -157,3 +157,15 @@ class TestProductModel(unittest.TestCase):
         # Check get back 5 products
         products= Product.all()
         self.assertEqual(len(products), 5)
+
+    def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
