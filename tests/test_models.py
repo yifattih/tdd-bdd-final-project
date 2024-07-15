7aeba93dc0d47325ba7c9498cac8dfbba43ec93b
yifattih <yifattih@proton.me>
2024-07-15 15:26:52 -0400

    microservice: test_find_by_availability implemented

--- tests/test_models.py
+++ tests/test_models.py
@@ -169,3 +169,15 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.name, name)
+    
+    def test_find_by_availability(self):
+        """It should Find Products by Availability"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        available = products[0].available
+        count = len([product for product in products if product.available == available])
+        found = Product.find_by_availability(available)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.available, available)
