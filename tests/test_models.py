a906f62811c1cee44dae6f4ba658f674b3d01fbb
yifattih <yifattih@proton.me>
2024-07-15 15:11:41 -0400

    microservice: test_update_a_product implemented

--- tests/test_models.py
+++ tests/test_models.py
@@ -115,4 +115,20 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found_product.name, product.name)
         self.assertEqual(found_product.description, product.description)
         self.assertEqual(found_product.price, product.price)
-
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        product= ProductFactory()
+        product.id= None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Change and save
+        product.description= "testing"
+        original_id= product.id
+        product.update()
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "testing")
+        # Fetch back, check ID is the same
+        products= Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].description, "testing")
