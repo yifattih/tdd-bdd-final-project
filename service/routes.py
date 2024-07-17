f6d8a7066b65a97f1f03b4c910676f13e7e69d7c
yifattih <yifattih@proton.me>
2024-07-17 13:43:44 -0400

    fix(test-routes): rout for reading a product implemented

--- service/routes.py
+++ service/routes.py
@@ -107,7 +107,20 @@ def create_products():
 ######################################################################
 
 #
-# PLACE YOUR CODE HERE TO READ A PRODUCT
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_products(product_id):
+    """Retrieve a single product based on id's"""
+    app.logger.info("Request to retrieve a product with id [%s]", product_id)
+
+    product = Product.find(product_id)
+
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id {product_id} was not found")
+
+    app.logger.info("Returning procut %s", product.name)
+    return product.serialize(), status.HTTP_200_OK
+
+
 #
 
 ######################################################################
